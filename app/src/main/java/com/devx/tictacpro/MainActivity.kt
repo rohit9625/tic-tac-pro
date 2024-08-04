a7d681fc36fd632147dd38ed482b355552d1ba06
Rohit Verma <rv17837@outlook.com>
2024-08-04 21:16:01 +0530

    navigation: add navigation to game screen
    Add type-safe navigation to navigate from home to game screen using parcelable

--- app/src/main/java/com/devx/tictacpro/MainActivity.kt
+++ app/src/main/java/com/devx/tictacpro/MainActivity.kt
@@ -8,18 +8,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.devx.tictacpro.presentation.GameScreen
-import com.devx.tictacpro.presentation.HomeScreen
+import androidx.navigation.toRoute
+import com.devx.tictacpro.presentation.Player
+import com.devx.tictacpro.presentation.PlayerNavType
+import com.devx.tictacpro.presentation.game.GameScreen
+import com.devx.tictacpro.presentation.home.HomeScreen
 import com.devx.tictacpro.presentation.auth.AuthScreen
 import com.devx.tictacpro.presentation.auth.AuthViewModel
+import com.devx.tictacpro.presentation.game.GameViewModel
 import com.devx.tictacpro.ui.theme.TicTacProTheme
-import com.google.firebase.FirebaseApp
 import kotlinx.serialization.Serializable
+import kotlin.reflect.typeOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,20 +53,31 @@ fun TicTacProNav(
                 AuthViewModel(TicTacProApp.appModule.authRepository)
             }
             val uiState by viewModel.uiState.collectAsState()
+
             AuthScreen(
                 state = uiState,
-                loginAsGuest = { viewModel.loginAsGuest {
-                    navController.navigate(Route.HomeScreen)
-                } },
+                onEvent = viewModel::onEvent,
+                navController = navController
             )
         }
 
         composable<Route.HomeScreen> {
-            HomeScreen()
+            HomeScreen(navController)
         }
 
-        composable<Route.GameScreen> {
-            GameScreen()
+        composable<Route.Game>(
+            typeMap = mapOf(typeOf<Player>() to PlayerNavType)
+        ) {
+            val args = it.toRoute<Route.Game>()
+            val viewModel = viewModel<GameViewModel>() {
+                GameViewModel(player1 = args.player1, player2 = args.player2)
+            }
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+            GameScreen(
+                gameState = uiState,
+                onEvent = viewModel::onEvent
+            )
         }
     }
 }
@@ -69,8 +85,10 @@ fun TicTacProNav(
 sealed interface Route {
     @Serializable
     data object AuthScreen: Route
+
     @Serializable
     data object HomeScreen: Route
+
     @Serializable
-    data object GameScreen: Route
+    data class Game(val player1: Player, val player2: Player): Route
 }
--- app/src/main/java/com/devx/tictacpro/presentation/Player.kt
+++ app/src/main/java/com/devx/tictacpro/presentation/Player.kt
@@ -1,10 +1,43 @@
 package com.devx.tictacpro.presentation
 
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import androidx.navigation.NavType
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
+@Parcelize
+@Serializable
 data class Player(
+    val id: Int,
     val name: String,
     @DrawableRes val avatar: Int,
     val turn: Char,
     var score: Int = 0
-)
+): Parcelable
+
+val PlayerNavType = object: NavType<Player>(isNullableAllowed = false) {
+    override fun get(bundle: Bundle, key: String): Player? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            bundle.getParcelable(key, Player::class.java)
+        } else {
+            bundle.getParcelable(key)
+        }
+    }
+
+    override fun parseValue(value: String): Player {
+        return Json.decodeFromString(value)
+    }
+
+    override fun put(bundle: Bundle, key: String, value: Player) {
+        bundle.putParcelable(key, value)
+    }
+
+    override fun serializeAsValue(value: Player): String {
+        return Json.encodeToString(value)
+    }
+}
